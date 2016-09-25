2dcf9311c89a63be94196952c21b8f3197c2cfa0
Christian Reichel <reichel.christian@gmail.com>
2016-09-25 14:47:14 -0300

    Almoste there

--- src/main/java/br/net/reichel/vivareal/domain/Property.java
+++ src/main/java/br/net/reichel/vivareal/domain/Property.java
@@ -4,5 +4,5 @@ package br.net.reichel.vivareal.domain;
  * Created by creichel on 9/25/16.
  */
 public class Property {
-    x
+
 }
--- src/main/java/br/net/reichel/vivareal/web/PropertiesController.java
+++ src/main/java/br/net/reichel/vivareal/web/PropertiesController.java
@@ -1,13 +1,36 @@
 package br.net.reichel.vivareal.web;
 
+import br.net.reichel.vivareal.domain.Property;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Created by creichel on 9/25/16.
  */
 @RestController
+@RequestMapping(value = "/{properties}")
 public class PropertiesController {
 
-    public Property
+    @RequestMapping(method = RequestMethod.POST)
+    public Property create(@Valid Property property) {
+        return new Property();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Property getProperty(@PathVariable Long id) {
+        return new Property();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Property> search(HttpRequest httpRequest) {
+        return Collections.emptyList();
+    }
 
 }
