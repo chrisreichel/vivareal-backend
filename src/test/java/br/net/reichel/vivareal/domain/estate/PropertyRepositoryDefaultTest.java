53e37b1bcc2a2102ae177aa2b7abf6d7e10ac709
Christian Reichel <ChristianReichel@fico.com>
2016-09-26 15:52:24 -0300

    Mais testes e mais ajuste d elogging

--- src/test/java/br/net/reichel/vivareal/domain/estate/PropertyRepositoryDefaultTest.java
+++ src/test/java/br/net/reichel/vivareal/domain/estate/PropertyRepositoryDefaultTest.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 
 import java.util.Set;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -46,6 +50,61 @@ public class PropertyRepositoryDefaultTest {
         assertTrue(7 == found.getLocation().getLatitude());
         assertTrue(7 == found.getLocation().getLongitude());
     }
+
+    @Test
+    public void shouldQueryAndFindTwoProps() throws Exception {
+        //Given
+        final PropertyRepositoryDefault repository = new PropertyRepositoryDefault();
+        repository.create(buildProperty(1, new Coordinate(1, 1)));
+        repository.create(buildProperty(2, new Coordinate(3, 3)));
+        repository.create(buildProperty(3, new Coordinate(5, 5)));
+        repository.create(buildProperty(4, new Coordinate(7, 7)));
+        repository.create(buildProperty(5, new Coordinate(9, 9)));
+        final BoundaryUpperLeft boundaryUpperLeft = new BoundaryUpperLeft(7, 9);
+        final BoundaryBottomRight boundaryBottomRight = new BoundaryBottomRight(9, 7);
+        //When
+        final Set<Property> foundProperties = repository.findByArea(boundaryUpperLeft, boundaryBottomRight);
+        //Then
+        assertTrue(2 == foundProperties.size());
+        assertThat(foundProperties, hasItem(hasProperty("id", is(4))));
+        assertThat(foundProperties, hasItem(hasProperty("id", is(5))));
+    }
+
+    @Test
+    public void shouldQueryAndFindNothing() throws Exception {
+        //Given
+        final PropertyRepositoryDefault repository = new PropertyRepositoryDefault();
+        repository.create(buildProperty(1, new Coordinate(1, 1)));
+        repository.create(buildProperty(2, new Coordinate(3, 3)));
+        repository.create(buildProperty(3, new Coordinate(5, 5)));
+        repository.create(buildProperty(4, new Coordinate(7, 7)));
+        repository.create(buildProperty(5, new Coordinate(17, 17)));
+        final BoundaryUpperLeft boundaryUpperLeft = new BoundaryUpperLeft(18, 32);
+        final BoundaryBottomRight boundaryBottomRight = new BoundaryBottomRight(64, 18);
+        //When
+        final Set<Property> foundProperties = repository.findByArea(boundaryUpperLeft, boundaryBottomRight);
+        //Then
+        assertTrue(foundProperties.isEmpty());
+    }
+
+    @Test
+    public void shouldQueryAndFindAll() throws Exception {
+        //Given
+        final PropertyRepositoryDefault repository = new PropertyRepositoryDefault();
+        repository.create(buildProperty(1, new Coordinate(1, 1)));
+        repository.create(buildProperty(2, new Coordinate(3, 3)));
+        repository.create(buildProperty(3, new Coordinate(5, 5)));
+        repository.create(buildProperty(4, new Coordinate(7, 7)));
+        repository.create(buildProperty(5, new Coordinate(9, 9)));
+        final BoundaryUpperLeft boundaryUpperLeft = new BoundaryUpperLeft(0, 9);
+        final BoundaryBottomRight boundaryBottomRight = new BoundaryBottomRight(9, 0);
+        //When
+        final Set<Property> foundProperties = repository.findByArea(boundaryUpperLeft, boundaryBottomRight);
+        //Then
+        assertTrue(5 == foundProperties.size());
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
 
     Property buildProperty(Integer id, Coordinate location) {
         final Property property = new Property();
